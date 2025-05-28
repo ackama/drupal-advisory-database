4d4c6002de9e21ef6469a6c7a3953ed691fc5250
Gareth Jones <gareth.jones@ackama.com>
2025-05-29 09:33:32 +1200

    feat: sort credits by name (#55)
    Sorting is good for predictability, and this also happens to reduce the
    diff in my upcoming rewrite of how we parse credits as that uses a `set`
    which has a random output order

--- scripts/generate_osv_advisories.py
+++ scripts/generate_osv_advisories.py
@@ -311,7 +311,7 @@ def get_credits_from_sa(credits: drupal.RichTextField) -> list[osv.Credit]:
       name = credit.split('">')[1].split('</a>')[0]
       credit_list.append({'name': name, 'contact': [href]})
 
-  return credit_list
+  return sorted(credit_list, key=lambda c: c['name'])
 
 
 def determine_composer_package_name(sa_advisory: drupal.Advisory) -> str:
